d7c18bc3f06ab096cce43c8c0377e544051e1394
sixx chung <sixx.chung@gmail.com>
2021-11-01 19:38:09 +0900

    11.01 office

--- app/common/config.py
+++ app/common/config.py
@@ -16,7 +16,7 @@ class Config:
 @dataclass
 class LocalConfig(Config):
     PROJ_RELOAD: bool = True
-    DB_URL:str = "mysql+pymysql://travis@localhost/notification_api?charset=utf8mb4"
+    DB_URL:str = "+pymysql://travis@localhost/notification_api?charset=utf8mb4"
     TRUSTED_HOSTS=["*"]
     ALLOW_SITE=["*"]
 
