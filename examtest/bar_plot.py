675591e52b3e2454b34612a590f195e62c542de1
sixx chung <sixx.chung@gmail.com>
2022-05-11 18:00:11 +0900

    bar chart

--- examtest/bar_plot.py
+++ examtest/bar_plot.py
@@ -1,13 +1,46 @@
 from datetime import date
+import numpy as np
 import plotly.express as px
 iris = px.data.iris()
 carshare = px.data.carshare()
 stocks = px.data.stocks()
 tips = px.data.tips()
+wind = px.data.wind()
+px.data.experiment()
+gapminder = px.data.gapminder()
 
-fig = px.scatter(tips, 
-    x="total_bill", y="tip", 
-    color="smoker")
+
+## Bar
+
+
+temp = gapminder[['country', 'iso_alpha', 'iso_num']]
+temp.drop_duplicates().shape[0] == temp.country.unique().shape[0]
+gminder= gapminder[gapminder.columns.difference(['iso_alpha', 'iso_num'])]
+ggminder = gminder.groupby(['continent', 'country', 'year'])
+ggminder = gminder.groupby(['continent', 'country', 'year'], as_index=False)
+ggminder = ggminder.agg('mean')
+#ggminder = ggminder.reset_index()
+
+d20 = ggminder[ggminder.year==2002].copy()
+d20 = d20.sort_values(by=['lifeExp'], ascending=False)
+d20['rank'] = list(range(1, len(d20.country)+1))
+
+
+trace1 = go.Bar(x=d20.country, y=d20.lifeExp,
+            text=round(d20.lifeExp,0) )
+layout = go.Layout(title="Bar Chart")
+fig = go.Figure([trace1], layout)
+fig.show()
+
+trace1 = go.Bar(x=d20.country, y=d20.lifeExp,
+            text=round(d20.lifeExp,0) )
+trace2 = go.
+layout = go.Layout(title="Bar Chart")
+fig = go.Figure([trace1], layout)
+fig.show()
+
+
+
 fig = px.scatter(tips, 
     x="total_bill", y="tip", 
     color="smoker", 
@@ -15,7 +48,6 @@ fig = px.scatter(tips,
 fig.show()
 
 import plotly.graph_objects as go
-import numpy as np
 
 trace = go.Scatter(
     x=tips['total_bill'],
@@ -43,3 +75,14 @@ for smoke in smoker:
 layout = go.Layout()
 fig = go.Figure(traces, layout)
 fig.show()
+
+
+gminder= gapminder[gapminder.columns.difference(['iso_alpha', 'iso_num'])]
+ggminder = gminder.groupby(['continent', 'country', 'year'])
+
+ggminder.first()
+ggminder.agg(lambda g:len(g))
+ggminder.filter(lambda g: g.country=='Algeria')
+
+
+
