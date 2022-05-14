7653db6dbccc3c0c6606d03368ab14b8820d1eee
sixx chung <sixx.chung@gmail.com>
2022-05-14 14:44:09 +0900

    uuu

--- examtest/multi.py
+++ examtest/multi.py
@@ -1,44 +1,48 @@
-from multiprocessing import Process, Queue
-#from threading import Thread
+
+
+import pytz
 import time
+from datetime import datetime
 
-def work(id, start, end, result):
-    total = 0
-    for i in range(start, end):
-        total += i
-    result.put(total)
-    #result.append(total)
-    return
+epochtime = 1642314376
 
 
-if __name__ == "__main__":
-    now = time.time()
-    START, END = 0, 1000000
 
-    result = Queue()
-    #result = list()
+datetimeobj1=datetime.fromtimestamp(epochtime)
+# datetime to timestamp
+timestamp1 = 
+timestamp2 = time.mktime(datetimeobj2.timetuple())
+datetime(2016, 8, 19).timestamp()
 
-    th1 = Process(target=work, args=(1, START,  END//2, result))
-    th2 = Process(target=work, args=(2, END//2, END,    result))
-    # th1 = Thread(target=work, args=(1, START,  END//2, result))
-    # th2 = Thread(target=work, args=(2, END//2, END,    result))
 
-    th1.start()
-    th2.start()
-    th1.join()
-    th2.join()
 
-    result.put('STOP')
 
-    print(f"Result: {time.time()-now}")
-from multiprocessing import Pool 
+# epoch to DateTime timezone
 
-def day_diff(i):
-    temp = mt_rt[mt_rt['clientId']==mt_cid[i]]['date']  
-    return (temp.values[-1] - temp.values[0]).days + 1
+tz = pytz.timezone('UTC')
+tz.localize(datetime.datetime(1970, 1, 1, 0, 0)).timestamp()
+# 0
 
-if __name__=='__main__':
-    start_time = time.time()
-    pool = Pool(processes=12)
-    result = pool.map(day_diff, range(0, len(mt_cid)))
-    print("- %s seconds -" % (time.time() - start_time))
+datetime.datetime(1970, 1, 1, 0, 0).timestamp()
+# -32400.0
+
+import pandas as pd
+import numpy as np
+df = pd.DataFrame([
+    [ 1,  2,  3,  4,  5],
+    [ 6,  7,  8,  9, 10],
+    [11, 12, 13, 14, 15],
+    [16, 17, 18, 19, 20]
+], index = ['ridx1', 'ridx2', 'ridx3', 'ridx4'],
+   columns=['colNm1', 'colNm2', 'colNm3', 'colNm4', 'colNm5'])
+
+df['행 합'] = df.sum(axis=1)
+df.loc['열 합']= df.sum(axis=0)
+
+df.append(df,ignore_index=True)
+
+df.apply(np.cumsum, axis=0)
+a = map(np.cumsum, df)
+
+a = [1,2,3,4,5,6,7]
+print(map(np.cumsum, a))
